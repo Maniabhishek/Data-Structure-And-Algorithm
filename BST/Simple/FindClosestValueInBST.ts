8f54ce340ec866f9d5eb95a934bcced2c65f9edb
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2023-05-16 16:30:15 +0530

    Create FindClosestValueInBST.ts
    find closest value in BST

---
+++ BST/Simple/FindClosestValueInBST.ts
@@ -0,0 +1,29 @@
+class BST {
+  value: number;
+  left: BST | null;
+  right: BST | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function findClosestValueInBst(tree: BST, target: number) {
+  // Write your code here.
+  return findClosestValueInBstHelper(tree, target, tree.value);
+}
+
+function findClosestValueInBstHelper(tree: BST|null, target: number, closest: number): number {
+  if(tree == null) return closest;
+  if(Math.abs(target-tree.value) < Math.abs(target-closest)) closest = tree.value
+
+  if(target > tree.value){
+    return findClosestValueInBstHelper(tree.right, target, closest)
+  } else if(target < tree.value) {
+    return findClosestValueInBstHelper(tree.left, target, closest)
+  }else {
+    return closest
+  }
+}
