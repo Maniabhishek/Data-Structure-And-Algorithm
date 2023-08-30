5aea93c65976e09be527e7a2e80b79239c4b442e
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2023-08-30 10:22:48 +0530

    Update h.moveElementToEnd.ts

--- Array/Easy/h.moveElementToEnd.ts
+++ Array/Easy/h.moveElementToEnd.ts
@@ -1,3 +1,15 @@
+/*
+  You're given an array of integers and an integer. Write a function that moves
+  all instances of that integer in the array to the end of the array and returns
+  the array.
+
+
+  The function should perform this in place (i.e., it should mutate the input
+  array) and doesn't need to maintain the order of the other integers.
+
+sampleInput = [2, 1, 2, 2, 2, 3, 4, 2]
+sampleOutput = [1, 3, 4 2, 2, 2, 2, 2]
+*/
 export function moveElementToEnd(array: number[], toMove: number) {
   // Write your code here.
   let toMoveIdx = 0;
