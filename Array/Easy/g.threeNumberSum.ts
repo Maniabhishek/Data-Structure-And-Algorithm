cdd9bdd8f70fc23ed9b04a945384107880ce34fb
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2023-08-30 09:29:51 +0530

    Create g.threeNumberSum.ts

---
+++ Array/Easy/g.threeNumberSum.ts
@@ -0,0 +1,24 @@
+type Triplet = [number, number, number];
+
+export function threeNumberSum(array: number[], targetSum: number): Triplet[] {
+  // Write your code here.
+  array = array.sort((a,b)=>(a-b))
+  let result: Triplet[] = []
+  for(let i = 0; i < array.length - 2 ; i++){
+    let left = i + 1;
+    let right = array.length - 1;
+    while(left < right){
+      let currentSum = array[i] + array[left] + array[right]
+      if(currentSum === targetSum){
+        result.push([array[i], array[left], array[right]])
+        left++
+        right--
+      }else if(currentSum > targetSum){
+        right--
+      }else {
+        left++
+      }
+    }
+  }
+  return result;
+}
