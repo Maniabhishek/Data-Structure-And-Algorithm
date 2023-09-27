fda7908a394d649b856d54fabd2e2ebd0132f13b
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2023-09-27 08:32:45 +0530

    Create 3.ceaserCipherEncryptor.ts

---
+++ strings/1.Easy/3.ceaserCipherEncryptor.ts
@@ -0,0 +1,29 @@
+/**
+  Given a non-empty string of lowercase letters and a non-negative integer
+  representing a key, write a function that returns a new string obtained by
+  shifting every letter in the input string by k positions in the alphabet,
+  where k is the key.
+
+  
+  Note that letters should "wrap" around the alphabet; in other words, the
+  letter z shifted by one returns the letter 
+
+  sample input: xyz , 2 
+  sample output: zab
+*/
+
+function ceaserCipherEncryptor(str: string, key: number){
+  const newCharArr: string[] = []
+  const newKey: number = key%26
+  for(const ch of string){
+    const newCh = getCharacterAfterShift(ch,newKey)
+    newCharArr.push(newCh)
+  }
+  return newCharArr.join('')
+}
+
+function getCharacterAfterShift(char: string, key: number)string{
+  const ascii = char.charCodeAt(0) + key
+  return ascii <= 122 ? string.fromCharCode(ascii) : string.fromCharCode(96 + ascii % 122)
+}
+  
