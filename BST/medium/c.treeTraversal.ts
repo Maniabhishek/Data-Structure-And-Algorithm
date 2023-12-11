620fc37119cff54ac289933b8fbc4730f671186f
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2023-12-11 19:02:27 +0530

    Create c.treeTraversal.ts

---
+++ BST/medium/c.treeTraversal.ts
@@ -0,0 +1,41 @@
+class Bst {
+  value: number;
+  left: Bst | null
+  right: Bst | null
+
+  constructor(value: number){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+//left -> root -> right
+export function InOrderTraversal(tree: Bst| null, array: number[]): number[]{
+    if(tree !== null){
+        InOrderTraversal(tree.left, array)
+        array.push(tree.value)
+        InOrderTraversal(tree.right, array)
+    }
+    return array
+}
+
+// root -> left -> right
+export function PreOrderTraversal(tree: Bst, array: number[]):number[] {
+    if(tree !== null){
+        array.push(tree.value)
+        PreOrderTraversal(tree.left, array)
+        PreOrderTraversal(tree.right, array)
+    }
+    return array
+}
+
+// left -> right -> root
+export function PostOrderTraversal(tree: Bst, array: number[]): number[]{
+    if(tree !==null){
+        PostOrderTraversal(tree.left, array)
+        PostOrderTraversal(tree.right, array)
+        array.push(tree.value)
+    }
+    return array
+}
