76c2c156a3ca88144c731f7610b7e11e8fd0c652
Maniabhishek <31520295+Maniabhishek@users.noreply.github.com>
2024-01-01 19:51:07 +0530

    Create j.BestTimeToBuyAndSellStock.ts

---
+++ Array/2.medium/j.BestTimeToBuyAndSellStock.ts
@@ -0,0 +1,27 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+*/
+
+function maxProfit(prices: number[]): number {
+    let maximum = 0;
+    let minimum = prices[0]
+
+    for(let i = 0; i < prices.length - 1; i++){
+        minimum = Math.min(minimum, prices[i])
+        maximum = Math.max(maximum, prices[i+1]-minimum)
+    }
+    return maximum
+};
